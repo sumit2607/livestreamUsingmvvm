db1acdc2d57da2c86aa79a61d293dd9b5ed1fe6d
sumit rai <sumit.rai@repcard.com>
2025-04-26 12:15:20 +0530

    jetpack completely added

--- app/src/main/java/com/example/livestreamusingmvvm/MainActivity.kt
+++ app/src/main/java/com/example/livestreamusingmvvm/MainActivity.kt
@@ -6,11 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.padding
@@ -25,9 +23,6 @@ import com.example.livestreamusingmvvm.ui.viewmodel.LiveStreamViewModel
 
 import android.content.Intent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -36,14 +31,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.*
 import com.example.livestreamusingmvvm.model.LiveStream
-import com.example.livestreamusingmvvm.ui.view.LiveStreamPlayerActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.material3.CircularProgressIndicator
 import com.example.livestreamusingmvvm.ui.view.ui.ui.LiveStreamPlayerComposeActivity
