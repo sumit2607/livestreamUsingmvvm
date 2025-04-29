cdebee1e071f6fd8af31975259f8e37c4aa9745d
sumit rai <sumit.rai@repcard.com>
2025-04-29 13:17:46 +0530

    Refreshing Live added real time

--- app/src/main/java/com/example/livestreamusingmvvm/ui/intent/LiveStreamsIntent.kt
+++ app/src/main/java/com/example/livestreamusingmvvm/ui/intent/LiveStreamsIntent.kt
@@ -4,7 +4,7 @@ package com.example.livestreamusingmvvm.ui.intent
 import com.example.livestreamusingmvvm.model.LiveStream
 
 sealed class LiveStreamsIntent {
-    object FetchLiveStreams : LiveStreamsIntent()
+    data object FetchLiveStreams : LiveStreamsIntent()
     data class RefreshLiveStreams(val forceRefresh: Boolean = true) : LiveStreamsIntent()
     data class FetchLiveStreamDetails(val streamId: String) : LiveStreamsIntent()
 }
